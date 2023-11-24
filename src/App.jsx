4b50103d5f9d8301a6ca4055836a324b653ba9bd
Adeoniye Adelu <adeluadeoniye14@gmail.com>
2023-11-24 04:09:43 +0100

    final setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import Dashboard from "./components/Dashboard";
 import FormPage from "./components/FormPage";
 import TablePage from "./components/TablePage";
-import DashboardLayout from "./layout/dashboardLayout";
+import Layout from "./layout/layout";
 
 function App() {
 
   return (
     <>
       <Routes>
-      <Route path="/" element={<DashboardLayout />}>
+      <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="home" element={<Dashboard />} />
           <Route path="form" element={<FormPage />} />
