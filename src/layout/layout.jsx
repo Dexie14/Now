3ce59a74a81701d14d480d77187218de22718ff6
Adeoniye Adelu <adeluadeoniye14@gmail.com>
2023-11-24 04:16:03 +0100

    final name setup

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-import SideBar from "../components/sideBar";
+import SideBar from "../components/SideBar";
 import Navbar from "../components/Navbar";
 import "./layout.css"
 import { useState } from "react";
